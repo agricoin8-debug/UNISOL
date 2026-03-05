8784662e3dfb3f4b4d9c45408d32463e08826546
agricoin8-debug <agricoin8@gmail.com>
2026-03-05 19:21:51 +0100

    Create replit.nix file with specified packages and environment variables

--- replit.nix
+++ replit.nix
@@ -1,13 +1,18 @@
-{  
-  "instructions": {  
-    "install": [  
-      "nodejs_20",  
-      "npm",  
-      "postgresql_16",  
-      "openssl",  
-      "python3",  
-      "gnumake",  
-      "gcc"  
-    ]  
-  }  
+{ 
+  "packages": [ 
+    "nodejs_20", 
+    "npm", 
+    "postgresql_16", 
+    "openssl", 
+    "python3", 
+    "gnumake", 
+    "gcc" 
+  ], 
+  "environment": { 
+    "PGDATA": "/var/lib/postgresql/data", 
+    "PGPORT": "5432", 
+    "PGUSER": "user", 
+    "PGPASSWORD": "password", 
+    "PGDATABASE": "database" 
+  } 
 }
