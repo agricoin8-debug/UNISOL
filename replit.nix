1eb4a154ab7db57fc1fb3814451624d36c55f2a3
agricoin8-debug <agricoin8@gmail.com>
2026-03-05 19:19:58 +0100

    Create replit.nix file with dependencies for Node.js, PostgreSQL, and others

---
+++ replit.nix
@@ -0,0 +1,13 @@
+{  
+  "instructions": {  
+    "install": [  
+      "nodejs_20",  
+      "npm",  
+      "postgresql_16",  
+      "openssl",  
+      "python3",  
+      "gnumake",  
+      "gcc"  
+    ]  
+  }  
+}
